1c72ed127167920e662598cd6dc6284cff885bd2
Uriel Gaston Molina <molinauriel870@gmail.com>
2026-07-13 10:34:08 -0300

    feat(servicios): added TODO methods

--- src/servicios/AsignacionService.java
+++ src/servicios/AsignacionService.java
@@ -1,5 +1,27 @@
 package servicios;
 
 public class AsignacionService {
+    private EmpleadoService empleadoService;
+    private ProyectoService proyectoService;
 
+    public AsignacionService(EmpleadoService empleadoService, ProyectoService proyectoService) {
+        this.empleadoService = empleadoService;
+        this.proyectoService = proyectoService;
+    }
+
+    public void asignarResponsableEnTarea(Integer numero, String titulo) {
+        //TODO
+    }
+
+    public void asignarResponsableMenosRetraso(Integer numero, String titulo) {
+        //TODO
+    }
+
+    public void reasignarEmpleadoEnProyecto(Integer numero, Integer legajo, String titulo) {
+        //TODO
+    }
+
+    public void reasignarEmpleadoConMenosRetraso(Integer numero, String titulo) {
+        //TODO
+    }
 }
--- src/servicios/ProyectoService.java
+++ src/servicios/ProyectoService.java
@@ -1,5 +1,12 @@
 package servicios;
 
+import java.util.List;
+
+import entidades.Empleado;
+import entidades.Estado;
+import entidades.Proyecto;
+import entidades.Tarea;
+
 public class ProyectoService {
 
     //TODO: evaluar parametros, placeholder: sin parámetros
@@ -22,6 +29,83 @@ public class ProyectoService {
         //TODO
     }
     
-    
+    public void finalizarProyecto(Integer numero, String fin) {
+        //TODDO
+    }
+
+    public double costoProyecto(Integer numero) {
+        //TODO
+        return -1;
+    }
+
+    //TODO: evaluar parametros, retornar id o objeto proyecto, placeholder: ID
+    public List<Integer> proyectosPendientes(Integer numero) {
+        //TODO
+        return null;
+    }
+
+        //TODO: evaluar parametros, retornar id o objeto proyecto, placeholder: ID
+    public List<Integer> proyectosActivos(Integer numero) {
+        //TODO
+        return null;
+    }
+
+        //TODO: evaluar parametros, retornar id o objeto proyecto, placeholder: ID
+    public List<Integer> proyectosFinalizados(Integer numero) {
+        //TODO
+        return null;
+    }
+
+    //Utilizar excepcion correspondiente "ProyectoFinalizadoException" || "ProyectoNoEncontradoException"
+    public boolean verificarProyecto(Integer ID) {
+        //TODO
+        return false;
+    }
+
+    public boolean estaFinalizado(Integer numero) {
+        //TODO
+        return false;
+    }
+
+    public List<Empleado> empleadosAsignadosAProyecto(Integer numero) {
+        //TODO
+        return null;
+    }
+
+    public List<Tarea> tareasProyectoNoAsignadas(Integer numero) {
+        //TODO
+        return null;
+    }
+
+    public List<Tarea> tareasDeUnProyecto(Integer numero) {
+        //TODO
+        return null;
+    }
+
+    public String consultarDomicilioProyecto(Integer numero) {
+        //TODO
+        return null;
+    }
+
+    public boolean verificarTarea(Proyecto proyecto, String titulo) {
+        //TODO
+        return false;
+    }
+
+    private Estado consultarProyecto(Integer numero) {
+        //TODO
+        return null;
+    }
+
+    private boolean verificarProyectoGeneral(Integer ID) {
+        //TODO
+        return false;
+    }
+
+    private boolean verificarFinalizacion(Integer ID) {
+        //TODO
+        return false;
+    }
+
 
 }
