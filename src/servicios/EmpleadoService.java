943f7e35cc7b277e839586e3c20dcf2c1307c277
Uriel Gaston Molina <molinauriel870@gmail.com>
2026-07-13 00:02:11 -0300

    feat(servicios): add TODO methods for services

--- src/servicios/EmpleadoService.java
+++ src/servicios/EmpleadoService.java
@@ -1,5 +1,45 @@
 package servicios;
 
+import java.util.List;
+import entidades.Empleado;
+
 public class EmpleadoService {
 
+    public void registrarEmpleado(String nombre, double valor) {
+        //TODO
+    }
+
+    public void registrarEmpleado(String nombre, double valor, String categoria) {
+        //TODO
+    }
+
+    public int consultarCantidadRetrasosEmpleado(Integer legajo) {
+        //TODO
+        return -1;
+    }
+
+    public boolean tieneRestrasos(Integer legajo) {
+        //TODO
+        return false;
+    }
+
+    public List<Empleado> empleados() {
+        //TODO
+        return null;
+    }
+
+    public List<Empleado> empleadosNoAsignados() {
+        //TODO
+        return null;
+    }
+
+    private void liberarEmpleado(Empleado empleado) {
+        //TODO
+    }
+
+    //TODO: Evaluar retorno empleado o legajo, placeholder: legajo
+    private int encontrarEmpleadoMenosRetraso() {
+        //TODO
+        return -1;
+    }
 }
--- src/servicios/ProyectoService.java
+++ src/servicios/ProyectoService.java
@@ -2,4 +2,26 @@ package servicios;
 
 public class ProyectoService {
 
+    //TODO: evaluar parametros, placeholder: sin parámetros
+    public void registrarProyecto() {
+        //TODO
+    }
+
+    //TODO: evaluar parametros, placeholder: sin parámetros
+    public void agregarTareaEnProyecto() {
+        //TODO
+    }
+
+    //TODO: evaluar parametros, placeholder: sin parámetros
+    public void registrarRetrasoEnTarea() {
+        //TODO
+    }
+
+    //TODO: evaluar parametros, placeholder: sin parámetros
+    public void finalizarTarea(Integer numero, String titulo) {
+        //TODO
+    }
+    
+    
+
 }
